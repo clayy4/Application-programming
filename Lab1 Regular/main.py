881236ea9dee985171036f0fad90fa0ffd5fae56
clayw <clayworks11337@gmail.com>
2025-09-20 14:39:21 +0400

    Расширен функционал

--- Lab1 Regular/main.py
+++ Lab1 Regular/main.py
@@ -4,18 +4,26 @@ import re
 import argparse
 
 #Находит все номера по заданному шаблону
-def count_correct_numbers(text):
-	pattern = r"(8|\+7)+[ (]*\d{3}[ )]*\d{3}[ -]*\d{2}[ -]*\d{2}"
-	count = len(re.findall(pattern, text))
-	return count
+def correct_numbers(text):
+	pattern = r"[8|\+7]+[ (]*\d{3}[ )]*\d{3}[ -]*\d{2}[ -]*\d{2}"
+	return re.findall(pattern, text)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--file_name", "-f", type=str, help = "file name")
+parser.add_argument("--read_file", "-rf", type=str, help = "file read name")
+parser.add_argument("--write_file", "-wf", type=str, help = "file write name")
 args = parser.parse_args()
 
+phone_numbers = []
 
+if args.read_file is not None:
+	with open(args.read_file, "r") as file:
+		text = file.read()	
+	phone_numbers = correct_numbers(text)
+	print("\nКоличество корректных номеров среди анкет: ",len(phone_numbers))
 
-with open(args.file_name, "r") as file:
-	text = file.read()	
+if args.write_file is not None:
+	with open(args.write_file, "w") as file:
+		for cur_number in phone_numbers:
+			file.write(cur_number + "\n")
+		print(f"Все номера успешно записаны в \"{args.write_file}\"\n")	
 
-print(count_correct_numbers(text))
